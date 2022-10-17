17de5389443817bcc8960b52235e6853153d9d9b
Victoria <vic_andrade_@hotmail.com>
2022-10-17 13:16:50 -0300

    Versão do projeto funcionando com bônus

---
+++ script.js
@@ -0,0 +1,184 @@
+let pratos = document.querySelectorAll(".prato");
+let bebidas = document.querySelectorAll(".bebida");
+let sobremesas = document.querySelectorAll(".sobremesa");
+
+let pratoSelecionado = document.querySelector(".pratos .escolhido");
+let bebidaSelecionada = document.querySelector(".bebidas .escolhido");
+let sobremesaSelecionada = document.querySelector(".sobremesas .escolhido");
+let pratoSelecionadoTexto = document.querySelector(".pratos .escolhido h1");
+let pratoSelecionadoPreco = document.querySelector(".pratos .escolhido .visto");
+let bebidaSelecionadaTexto = document.querySelector(".bebidas .escolhido h1");
+let bebidaSelecionadaPreco = document.querySelector(
+  ".bebidas .escolhido .visto"
+);
+let sobremesaSelecionadaTexto = document.querySelector(
+  ".sobremesas .escolhido h1"
+);
+let sobremesaSelecionadaPreco = document.querySelector(
+  ".sobremesas .escolhido .visto"
+);
+let nome;
+let endereço;
+let total;
+
+function selecionarPrato(prato) {
+  if (pratoSelecionado !== null) {
+    console.log(pratoSelecionado);
+    pratoSelecionado.classList.remove("escolhido");
+  }
+
+  prato.classList.add("escolhido");
+  pratoSelecionado = document.querySelector(".pratos .escolhido");
+  habilitarBotao();
+}
+
+function selecionarBebida(bebida) {
+  if (bebidaSelecionada !== null) {
+    console.log(bebidaSelecionada);
+    bebidaSelecionada.classList.remove("escolhido");
+  }
+
+  bebida.classList.add("escolhido");
+  bebidaSelecionada = document.querySelector(".bebidas .escolhido");
+  habilitarBotao();
+}
+
+function selecionarSobremesa(sobremesa) {
+  if (sobremesaSelecionada != null) {
+    console.log(sobremesaSelecionada);
+    sobremesaSelecionada.classList.remove("escolhido");
+  }
+
+  sobremesa.classList.add("escolhido");
+  sobremesaSelecionada = document.querySelector(".sobremesas .escolhido");
+  habilitarBotao();
+}
+
+function habilitarBotao() {
+  let botao = document.querySelector(".botao");
+  botao.disabled = true;
+  if (
+    pratoSelecionado != null &&
+    bebidaSelecionada != null &&
+    sobremesaSelecionada != null
+  ) {
+    console.log("entrei2");
+    botao.innerHTML = `Finalizar pedido`;
+    botao.classList.add("ativado");
+    botao.disabled = false;
+  } else {
+    botao.disabled = true;
+  }
+}
+
+function fecharPedido() {
+  console.log("entrei no pedido");
+  nome = prompt("Qual o seu nome?");
+  endereço = prompt("Qual o endereço completo para entrega?");
+  let pedido = document.querySelector(".container");
+  let menu = document.querySelector("main");
+  let topo = document.querySelector("header");
+  let botao = document.querySelector("footer");
+
+  pedido.classList.remove("escondido");
+  botao.classList.add("escondido");
+  menu.classList.add("desfoco");
+  topo.classList.add("desfoco");
+  pratos.disabled = true;
+  bebidas.disabled = true;
+  sobremesas.disabled = true;
+
+  pratoSelecionadoTexto = document.querySelector(
+    ".pratos .escolhido h1"
+  ).textContent;
+  pratoSelecionadoPreco = document.querySelector(
+    ".pratos .escolhido .visto"
+  ).textContent;
+  bebidaSelecionadaTexto = document.querySelector(
+    ".bebidas .escolhido h1"
+  ).textContent;
+  bebidaSelecionadaPreco = document.querySelector(
+    ".bebidas .escolhido .visto"
+  ).textContent;
+  sobremesaSelecionadaTexto = document.querySelector(
+    ".sobremesas .escolhido h1"
+  ).textContent;
+  sobremesaSelecionadaPreco = document.querySelector(
+    ".sobremesas .escolhido .visto"
+  ).textContent;
+
+  console.log(total);
+  somar();
+  item = `<div class="pedido">
+    <h1>Confirme seu pedido</h1>
+    <div class='pratoSelecionado'><p>${pratoSelecionadoTexto}</p><p>${pratoSelecionadoPreco}</p></div>
+    <div class='bebidaSelecionada'><p>${bebidaSelecionadaTexto}</p><p>${bebidaSelecionadaPreco}</p></div>
+    <div class='sobremesaSelecionada'><p>${sobremesaSelecionadaTexto}</p><p>${sobremesaSelecionadaPreco}</p></div>
+    <div class='total'><span>TOTAL</span><strong>R$ ${total}</strong></div>
+    <div class='botoes'>
+    <button class='certo' onclick="enviarPedido()">Tudo certo, pode pedir!</button>
+    <button class='cancelar' onclick="cancelar()">Cancelar</button>
+    </div>
+    </div>`;
+
+  pedido.innerHTML += item;
+  console.log("fecharPedido" + pratoSelecionadoTexto);
+  console.log("fecharPedido" + bebidaSelecionadaTexto);
+  console.log("fecharPedido" + sobremesaSelecionadaTexto);
+}
+
+function somar() {
+  let sobremesaSelecionada1 = sobremesaSelecionadaPreco.replace(",", ".");
+  let bebidaSelecionada1 = bebidaSelecionadaPreco.replace(",", ".");
+  let pratoSelecionado1 = pratoSelecionadoPreco.replace(",", ".");
+  let pratoSelecionado2 = pratoSelecionado1.replace("R$", "");
+  let bebidaSelecionada2 = bebidaSelecionada1.replace("R$", "");
+  let sobremesaSelecionada2 = sobremesaSelecionada1.replace("R$", "");
+
+  let soma =
+    Number(pratoSelecionado2) +
+    Number(bebidaSelecionada2) +
+    Number(sobremesaSelecionada2);
+  let soma2 = soma.toFixed(2);
+  total = soma2.replace(".", ",");
+  console.log(total);
+}
+
+function enviarPedido() {
+  let mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${pratoSelecionadoTexto}
+    - Bebida: ${bebidaSelecionadaTexto}
+    - Sobremesa: ${sobremesaSelecionadaTexto}
+    Total: R$ ${total}
+    
+    Nome: ${nome}
+    Endereço: ${endereço}`;
+
+  mensagem = encodeURIComponent(mensagem);
+
+  const link = `https://wa.me/7591078928?text=${mensagem}`;
+
+  window.open(link);
+  console.log("enviarPedido" + pratoSelecionadoTexto);
+  console.log("enviarPedido" + bebidaSelecionadaTexto);
+  console.log("enviarPedido" + sobremesaSelecionadaTexto);
+  console.log(nome);
+  console.log(endereço);
+  console.log(mensagem);
+}
+
+function cancelar() {
+  let pedido = document.querySelector(".container");
+  let menu = document.querySelector("main");
+  let topo = document.querySelector("header");
+  let botao = document.querySelector("footer");
+
+  pedido.classList.add("escondido");
+  botao.classList.remove("escondido");
+  menu.classList.remove("desfoco");
+  topo.classList.remove("desfoco");
+  pedido.innerHTML = "";
+  pratos.disabled = false;
+  bebidas.disabled = false;
+  sobremesas.disabled = false;
+}
